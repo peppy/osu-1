b4482cf8319fe81143903638b46f13e2477864fa
Dean Herbert <pe@ppy.sh>
2019-10-01 14:15:17 +0800

    Display bullets one at a time

--- osu.Presentation/BulletSlide.cs
+++ osu.Presentation/BulletSlide.cs
@@ -1,15 +1,22 @@
+using System.Linq;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
+using osu.Framework.Input.Events;
 using osu.Game.Graphics;
+using osuTK;
+using osuTK.Input;
 
 namespace osu.Presentation
 {
     public class BulletSlide : SlideWithTitle
     {
+        private readonly string[] bullets;
+        private readonly TextFlowContainer textFlow;
+
         public BulletSlide(string title, string[] bullets)
             : base(title)
         {
-            TextFlowContainer textFlow;
+            this.bullets = bullets;
 
             Content.Add(textFlow = new TextFlowContainer(s => s.Font = OsuFont.Default.With(size: 50))
             {
@@ -18,8 +25,35 @@ namespace osu.Presentation
                 LineSpacing = 0.8f,
             });
 
-            foreach (var b in bullets)
+            showBullets(0);
+        }
+
+        private bool showBullets(int count)
+        {
+            bulletsDisplayed = MathHelper.Clamp(count, 0, bullets.Length);
+            textFlow.Clear();
+            foreach (var b in bullets.Take(bulletsDisplayed))
                 textFlow.AddParagraph("• " + b);
+
+            return bulletsDisplayed == count;
+        }
+
+        private int bulletsDisplayed = 1;
+
+        protected override bool OnKeyDown(KeyDownEvent e)
+        {
+            if (e.Repeat) return false;
+
+            switch (e.Key)
+            {
+                case Key.Left:
+                    return showBullets(bulletsDisplayed - 1);
+
+                case Key.Right:
+                    return showBullets(bulletsDisplayed + 1);
+            }
+
+            return base.OnKeyDown(e);
         }
     }
 }
