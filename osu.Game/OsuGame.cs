b6f5675f508f447da918a56eb2ae05e7cc13e88d
Dean Herbert <pe@ppy.sh>
2019-12-02 16:14:52 +0900

    Fix currenTrackCompleted not being run on main thread

--- osu.Game/OsuGame.cs
+++ osu.Game/OsuGame.cs
@@ -421,11 +421,11 @@ namespace osu.Game
             nextBeatmap?.LoadBeatmapAsync();
         }
 
-        private void currentTrackCompleted()
+        private void currentTrackCompleted() => Schedule(() =>
         {
             if (!Beatmap.Value.Track.Looping && !Beatmap.Disabled)
                 musicController.NextTrack();
-        }
+        });
 
         #endregion
 
