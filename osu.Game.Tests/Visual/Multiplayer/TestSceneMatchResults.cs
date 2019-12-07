eed25e55dd9f9f4e0213dd17373f43bab9b70b9d
Dean Herbert <pe@ppy.sh>
2019-12-08 02:00:02 +0900

    Remove unused using

--- osu.Game.Tests/Visual/Multiplayer/TestSceneMatchResults.cs
+++ osu.Game.Tests/Visual/Multiplayer/TestSceneMatchResults.cs
@@ -4,7 +4,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.EntityFrameworkCore.Internal;
 using osu.Framework.Allocation;
 using osu.Game.Beatmaps;
 using osu.Game.Online.API;
